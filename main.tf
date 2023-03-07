c2bd8cc1c0121c9c9f89b6bfb6ee7a584799dbf6
cjoblin <127150666+cjoblin@users.noreply.github.com>
2023-03-07 14:08:49 +1300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,20 @@
 # Configure the provider
 provider "azurerm" {
-  version = "=1.20.0"
+  version = "=4.20.0"
 }
 
-resource "blah" {
-  ""
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "Australia East"
+}
+
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
 }
 
 invalid{
