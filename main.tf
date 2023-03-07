2a3bb7d6ba541e5c4da1a3a9810725f20489a550
cjoblin <127150666+cjoblin@users.noreply.github.com>
2023-03-07 17:55:27 +1300

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,18 +12,47 @@ terraform {
 provider "azurerm" {
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "example-rg" {
   name     = "example-resources"
   location = "Australia East"
 }
 
 
 # Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "example-vnet" {
   name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example-rg.name
+  location            = azurerm_resource_group.example-rg.location
   address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "example-snet" {
+  name                 = "example-subnetname"
+  resource_group_name  = azurerm_resource_group.example-vnet.name
+  virtual_network_name = azurerm_virtual_network.example-vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
+}
+
+
+#create a storage accoutn with public access to test tfsec
+resource "azurerm_storage_account" "example-stor" {
+  name                = "ex-storageaccountname"
+  resource_group_name = azurerm_resource_group.example-rg.name
+
+  location                 = azurerm_resource_group.example-rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  network_rules {
+    default_action             = "Allow"
+    ip_rules                   = ["0.0.0.0/0"]
+    virtual_network_subnet_ids = [azurerm_subnet.example-snet.id]
+  }
+
+  tags = {
+    environment = "staging"
+  }
 }
 
 #invalid{
