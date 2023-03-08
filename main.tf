deaa1f278f7901aeedc70e88d4d460e94c2e407a
cjoblin <127150666+cjoblin@users.noreply.github.com>
2023-03-09 11:29:14 +1300

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "azurerm_virtual_network" "example-vnet" {
 
 resource "azurerm_subnet" "example-snet" {
   name                 = "example-subnetname"
-  resource_group_name  = azurerm_resource_group.example-vnet.name
+  resource_group_name  = azurerm_resource_group.example-rg.name
   virtual_network_name = azurerm_virtual_network.example-vnet.name
   address_prefixes     = ["10.0.2.0/24"]
   service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
